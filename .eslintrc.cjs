f67db12f1cb1eb071ec052bbba3db3a6a6114063
Joe Lin <joelin3478@gmail.com>
2023-01-12 21:07:53 -0500

    Add ESLint and VS Code configs

---
+++ .eslintrc.cjs
@@ -0,0 +1,10 @@
+module.exports = {
+  extends: [
+    'next/core-web-vitals',
+    'next/babel',
+    'plugin:@typescript-eslint/recommended',
+    'eslint:recommended'
+  ],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint']
+};
