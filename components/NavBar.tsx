0a079991d69751a9469447293b9d10131aa941ec
Joe Lin <joelin3478@gmail.com>
2023-01-12 21:08:30 -0500

    Add ESLint, Prettier, and VS Code configs; update package.json; add Postgres; architecture complete.

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,10 +1,16 @@
-import { Box, Container } from '@chakra-ui/react';
+import { Box, Container, Stack } from '@chakra-ui/react';
 import { DarkMode } from './DarkMode';
 
 export const NavBar = (props:any) => {
   return (
     <Box as='nav' w='100%' css={{ backDropFilter: 'blur(10px)' }}{...props}>
       <Container display="flex" p={2} maxW="container.xl">
+        <Stack direction={{ base: "column", md: "row" }}
+            display={{ base: "none", md: "flex" }}
+            width={{ base: "full", md: "auto" }}
+            alignItems="center"
+            flexGrow={1}
+            mt={{ base: 5, md: 0 }}/>
         <DarkMode/>
       </Container>
   </Box>
