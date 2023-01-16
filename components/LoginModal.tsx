4faf49cfb822dc0a557f5fa4d74499360fa1647e
Thomas Broadwater <tgbroadh2o@gmail.com>
2023-01-16 13:37:29 -0500

    Add login button skeleton code

--- components/LoginModal.tsx
+++ components/LoginModal.tsx
@@ -1,9 +1,35 @@
 import { useDisclosure, Button, Modal, ModalOverlay, ModalContent, ModalHeader, ModalBody, ModalCloseButton, ModalFooter, Editable, EditablePreview, EditableInput, Stack, IconButton, ButtonGroup, Input, FormControl, FormLabel } from '@chakra-ui/react'
 import { AiOutlineUser } from 'react-icons/ai'
 import { signIn } from 'next-auth/react'
+import { useRef } from 'react'
 
 export const LoginModal = () => {
   const { isOpen, onOpen, onClose } = useDisclosure()
+
+  // useState or useRef?
+  const user = useRef<HTMLInputElement>(null)
+  const pass = useRef<HTMLInputElement>(null)
+
+  // login handler for custom credentials
+  const handleLogin = async (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault()
+    if (!user.current || !pass.current) return
+
+    const result = await signIn('credentials', {
+      redirect: false,
+      username: user.current.value,
+      password: pass.current.value,
+    })
+
+    console.log('login result:', result)
+
+    // check if login was successful
+    if (result && !result.error && result.ok) {
+      // do something on success
+    } else {
+      // do something for invalid credentials
+    }
+  }
 
   return (
     <>
@@ -17,13 +43,13 @@ export const LoginModal = () => {
 
           <ModalBody>
             <Stack direction='column' spacing='5'>
-              <FormControl isRequired>
+              <FormControl>
                 <FormLabel>Username</FormLabel>
-                <Input placeholder='Username' bg='black' borderRadius='5'/>
+                <Input ref={user} placeholder='Username' bg='black' borderRadius='5'/>
               </FormControl>
-              <FormControl isRequired>
+              <FormControl>
                 <FormLabel>Password</FormLabel>
-                <Input placeholder='Password' bg='black' borderRadius='5'/>
+                <Input ref={pass} type='password' placeholder='Password' bg='black' borderRadius='5'/>
               </FormControl>
             </Stack>
           </ModalBody>
@@ -32,7 +58,7 @@ export const LoginModal = () => {
             <ButtonGroup>
               <Button>Sign Up</Button>
               <Button onClick={() => signIn('google')}>Login with Google</Button>
-              <Button >Login</Button>
+              <Button onClick={handleLogin}>Login</Button>
             </ButtonGroup>
           </ModalFooter>
 
