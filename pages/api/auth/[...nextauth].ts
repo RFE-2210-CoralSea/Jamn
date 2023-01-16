dc3fdda0f3742947741f9bb482fff2681531b1a3
Thomas Broadwater <tgbroadh2o@gmail.com>
2023-01-16 12:33:38 -0500

    Add pseudocode for creating OAuth accounts

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,7 +1,31 @@
-import NextAuth from 'next-auth'
+import NextAuth, { NextAuthOptions } from 'next-auth'
 import GoogleProvider from 'next-auth/providers/google'
 
-export const authOptions = {
+export const authOptions: NextAuthOptions = {
+  session: {
+    strategy: 'jwt'
+  },
+  callbacks: {
+    async signIn({ user, account, credentials }) {
+      // user is equivalent to session object, with an id
+      console.log('user', user)
+      // account stores provider info
+      console.log('account', account)
+      // may be useful for custom credentials
+      console.log('credentials', credentials)
+
+      // an account needs to be created on the DB
+      if (account?.provider === 'google') {
+        console.log('checking account')
+        /**
+         * check DB for google account
+         * if account doesn't exist
+         *  create account
+         */
+      }
+      return true
+    }
+  },
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
