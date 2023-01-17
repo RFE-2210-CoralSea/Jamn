9bd1b968d99212029745cde48fef9766faf30a94
Thomas Broadwater <tgbroadh2o@gmail.com>
2023-01-17 16:02:50 -0500

    Add button to nav bar

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,6 +1,7 @@
 import { Flex, Stack, Heading } from '@chakra-ui/react'
 import { DarkMode } from './DarkMode'
 import { Login } from './Login'
+import SignOut from './SignOut'
 import { BandModal } from './BandModal'
 import { RecordingModal } from './RecordingModal'
 import { useSession } from 'next-auth/react'
@@ -26,9 +27,9 @@ export const NavBar = () => {
     <Flex w='100vw' justifyContent='space-between' p={2}>
       <Heading><Link href="/">Musi 🎸</Link></Heading>
       <Stack direction='row'>
-          <Login/>
           <BandModal/>
           <RecordingModal/>
+          <SignOut/>
           <DarkMode/>
       </Stack>
     </Flex>
