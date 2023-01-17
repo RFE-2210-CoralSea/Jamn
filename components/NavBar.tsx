569e65d5c78eaa3265cd0baa6357e78a61f46bce
Thomas Broadwater <tgbroadh2o@gmail.com>
2023-01-17 15:48:23 -0500

    Removed login modal to just use Google OAuth

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,6 +1,6 @@
 import { Flex, Stack, Heading } from '@chakra-ui/react'
 import { DarkMode } from './DarkMode'
-import { LoginModal } from './LoginModal'
+import { Login } from './Login'
 import { BandModal } from './BandModal'
 import { RecordingModal } from './RecordingModal'
 
@@ -9,7 +9,7 @@ export const NavBar = () => {
     <Flex w='100vw' justifyContent='space-between' p={2}>
       <Heading>Musi 🎸</Heading>
       <Stack direction='row'>
-          <LoginModal/>
+          <Login/>
           <BandModal/>
           <RecordingModal/>
           <DarkMode/>
