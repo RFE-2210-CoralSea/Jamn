6e6c59d290d2a8f77ea6c77a7b0f81334b1a6d2b
Kchen1515 <kychen249@gmail.com>
2023-01-17 09:49:11 -0500

    moved svg to index file

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,61 @@
 import Head from 'next/head'
 import { Inter } from '@next/font/google'
-import { Container } from '@chakra-ui/react'
+import { Container, Box, Text } from '@chakra-ui/react'
 import { NavBar } from '../components/NavBar'
 
 const inter = Inter({ subsets: ['latin'] })
 
+const innerBoxStyles = {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    textAlign: 'center',
+    boxSize: 'full',
+    color: 'white',
+    textShadow: '0 0 20px black',
+    fontWeight: 'bold',
+    fontSize: '20px',
+  }
+
+
 export default function Home() {
+
   return (
     <>
       <Head>
         <title>Blue-Ocean</title>
       </Head>
 
-      <Container>
-        {/* <NavBar/> */}
-        Hello WOrld
-      </Container>
+      <Box
+        height="100vh"
+        backgroundImage="url('animatedBackground.svg')"
+        backgroundPosition="center"
+        backgroundRepeat="no-repeat"
+        >
+        <Container>
+          <NavBar/>
+        </Container>
+        <Box
+          height="50%"
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
+        >
+          <Container
+            border="1px"
+            width="200px"
+            height="200px"
+            bgColor="transparent"
+          >
+          </Container>
+          <Container border="1px" textStyle="center">
+            <Text textAlign="center">Possible Slogan</Text>
+          </Container>
+
+
+        </Box>
+
+      </Box>
     </>
   )
 }
