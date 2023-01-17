7635fbaaa16487be529ec8063062e50ac05b001c
Ivan Day <ivanday@utexas.edu>
2023-01-17 13:15:02 -0600

    Add bands endpoint with sample return data

--- pages/api/bands.ts
+++ pages/api/bands.ts
@@ -2,17 +2,23 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 
 type Data = {
   name: string,
-  band: string,
-  text: string,
-  audio: string,
-  pdf: string
+  image: string,
+  description: string,
+  posts: [],
+  members: []
 };
 
 export default function handler (
   req: NextApiRequest,
-  res: NextApiResponse<Data[]>
+  res: NextApiResponse<Data>
 ) {
   if (req.method === 'GET') {
+    res.send({
+      name: 'test',
+      image: 'test',
+      description: 'test',
+      posts: [],
+      members:[]});
     res.status(200).end();
   } else if (req.method === 'POST') {
     res.status(201).end();
