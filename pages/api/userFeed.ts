98b1c50eff12e2da48d59f92a197ae880a28cb38
Ivan Day <ivanday@utexas.edu>
2023-01-17 15:42:17 -0600

    update sample data

--- pages/api/userFeed.ts
+++ pages/api/userFeed.ts
@@ -7,37 +7,53 @@ type Comment = {
   date: string
 }
 
-type Data = {
+type Post = {
   name: string,
   band: string,
-  text: string,
   audio: string,
   pdf: string,
   date: string,
+  text: string,
   comments: Comment[]
+}
+
+type User = {
+  name: string,
+  description: string,
+  instruments: string[],
+  image: string,
+  posts: Post[],
 };
 
 const SampleData = {
   name: 'Ivan',
-  band: 'MySickBand',
-  text: 'hello world',
-  audio: 'sampleaudio.wav',
-  pdf: 'samplepdf.pdf',
-  date: '01/17/2023 @ 8:09pm',
-  comments: [{
-    name: 'Joe',
-    profile_picture: 'sampleprofpic.jpg',
-    text: 'hello comments',
-    date: '01/17/2023 @ 8:10pm'
-  }]
+  description: 'hello world',
+  instruments: ['Cello', 'Piano', 'Drums'],
+  image: 'testprofilepicture.jpg',
+  posts: [
+    {
+      name: 'Joe',
+      band: 'Super Sick Band',
+      audio: 'testaudio.wav',
+      pdf: 'testpdf.pdf',
+      date: '01/17/2023 @ 8:09pm',
+      text: 'Hello user feed',
+      comments: [{
+        name: 'Darrien',
+        profile_picture: 'sampleprofpic.jpg',
+        text: 'hello comments',
+        date: '01/17/2023 @ 8:10pm'
+      }]
+    }
+  ]
 }
 
 export default function handler (
   req: NextApiRequest,
-  res: NextApiResponse<Data[]>
+  res: NextApiResponse<User>
 ) {
   if (req.method === 'GET') {
-    res.send([SampleData]);
+    res.send(SampleData);
     res.status(200).end();
   }
 };
