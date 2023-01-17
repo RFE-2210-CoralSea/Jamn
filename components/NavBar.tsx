2e1c12a24e76e2ba4905d1012738df72f3427cfc
Thomas Broadwater <tgbroadh2o@gmail.com>
2023-01-17 16:01:11 -0500

    Add sign out button

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -3,11 +3,28 @@ import { DarkMode } from './DarkMode'
 import { Login } from './Login'
 import { BandModal } from './BandModal'
 import { RecordingModal } from './RecordingModal'
+import { useSession } from 'next-auth/react'
+import Link from 'next/link'
 
 export const NavBar = () => {
+  const { data: session, status } = useSession()
+
+  // user is not logged in
+  if (status === 'unauthenticated' || !session) {
+    return (
+      <Flex w='100vw' justifyContent='space-between' p={2}>
+        <Heading><Link href="/">Musi 🎸</Link></Heading>
+        <Stack direction='row'>
+            <Login/>
+            <DarkMode/>
+        </Stack>
+      </Flex>
+    )
+  }
+
   return (
     <Flex w='100vw' justifyContent='space-between' p={2}>
-      <Heading>Musi 🎸</Heading>
+      <Heading><Link href="/">Musi 🎸</Link></Heading>
       <Stack direction='row'>
           <Login/>
           <BandModal/>
