3e644d44557b319f6afa416749465dad4e8ecc28
Ivan Day <ivanday@utexas.edu>
2023-01-17 13:36:38 -0600

    Add more data types and further flesh out band page endpoint

--- pages/api/bands.ts
+++ pages/api/bands.ts
@@ -1,24 +1,37 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 
-type Data = {
+type Post = {
+  name: string,
+  text: string,
+  audio: string,
+  pdf: string,
+}
+
+type Member = {
+  name: string,
+  role: string
+}
+
+type BandPage = {
   name: string,
   image: string,
   description: string,
-  posts: [],
-  members: []
+  posts: Post[],
+  members: Member[]
 };
 
 export default function handler (
   req: NextApiRequest,
-  res: NextApiResponse<Data>
+  res: NextApiResponse<BandPage>
 ) {
   if (req.method === 'GET') {
+    //this request will take in a band name, query db for the band id, then query for posts and users related to that band id
     res.send({
-      name: 'test',
-      image: 'test',
-      description: 'test',
-      posts: [],
-      members:[]});
+      name: 'Band name placeholder',
+      image: 'bandimage.jpg',
+      description: 'This is a band. We make music.',
+      posts: [{name: 'ivan', text: 'my first post!', audio: 'sampleaudio.wav', pdf: 'samplepdf.pdf'}],
+      members:[{name: 'ivan', role: 'administrator'}]});
     res.status(200).end();
   } else if (req.method === 'POST') {
     res.status(201).end();
