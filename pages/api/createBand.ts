5c4e78044e6d779f6bb68e37faf5a38b5fa4f1b8
Ivan Day <ivanday@utexas.edu>
2023-01-18 11:31:02 -0600

    Add create Band api endpoint

---
+++ pages/api/createBand.ts
@@ -0,0 +1,19 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import prisma from '../../lib/Prisma';
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  console.log(req);
+  prisma.users.create({
+    data: req.body
+  })
+  .then((response) => {
+    res.status(200).end();
+  })
+  .catch((err) => {
+    res.send(err);
+    res.status(400).end();
+  })
+}
