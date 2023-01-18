b5308c3b0e94b24fdba3e63d56170d283fbb3b92
Ivan Day <ivanday@utexas.edu>
2023-01-18 14:40:28 -0600

    handle requests other than POST

--- pages/api/createBand.ts
+++ pages/api/createBand.ts
@@ -1,20 +1,31 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import prisma from '../../lib/Prisma';
+import { unstable_getServerSession } from 'next-auth';
+import { authOptions } from './auth/[...nextauth]';
 
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  console.log(req);
-  prisma.bands.create({
-    data: req.body
-  })
+  if (req.method !== 'POST') {
+    res.status(405).send({message: 'Only POST requests allowed!'})
+    res.end();
+  }
+  //console.log(req);
+  unstable_getServerSession(req, res, authOptions)
   .then((response) => {
     console.log(response);
-    res.status(200).end();
   })
-  .catch((err) => {
-    res.send(err);
-    res.status(400).end();
-  })
+  // prisma.bands.create({
+  //   data: req.body
+  // })
+  // .then((response) => {
+  //   console.log(response);
+
+  //   res.status(200).end();
+  // })
+  // .catch((err) => {
+  //   res.send(err);
+  //   res.status(400).end();
+  // })
 }
