a413c6d2456b5499f555e4c47e3f9e1ce5c50e71
Thomas Broadwater <tgbroadh2o@gmail.com>
2023-01-17 21:23:43 -0500

    Get very basic recording functionality

--- components/RecordingModal.tsx
+++ components/RecordingModal.tsx
@@ -1,10 +1,52 @@
 import { Modal, ModalOverlay, ModalContent, ModalHeader, ModalFooter, ModalBody, ModalCloseButton } from '@chakra-ui/react'
 import { IconButton, Button, ButtonGroup, Tooltip } from '@chakra-ui/react'
-import { AiOutlineCustomerService } from 'react-icons/ai'
+import { AiOutlineCustomerService, AiOutlinePlayCircle } from 'react-icons/ai'
 import { useDisclosure } from '@chakra-ui/react'
+import { useEffect, useState } from 'react'
 
 export const RecordingModal = () => {
   const { isOpen, onOpen, onClose } = useDisclosure()
+  const [ recording, setRecording ] = useState(false)
+  const [ recorder, setRecorder ] = useState<MediaRecorder | null>(null)
+  const [ url, setUrl ] = useState('')
+
+  useEffect(() => {
+    navigator.mediaDevices.getUserMedia({ audio: true })
+      .then((stream) => {
+        setRecorder(new MediaRecorder(stream))
+      })
+      .catch(console.error)
+  }, [])
+
+  useEffect(() => {
+    if (recorder) {
+      recorder.ondataavailable = (event) => {
+        console.log('test')
+        setUrl(URL.createObjectURL(event.data))
+      }
+    }
+  }, [recorder])
+
+  // begin recording
+  const record = async () => {
+    if (!recorder) return
+    if (recording) {
+      console.log('stop', url)
+      recorder.stop()
+      const audio = new Audio(url)
+      audio.play()
+      setRecording(false)
+    } else {
+      setRecording(true)
+      recorder.start()
+    }
+  }
+
+  // submit audio to db
+  const submit = () => {
+    console.log('store audio in db')
+  }
+
   return (
     <>
     <Tooltip hasArrow label='Record a song!'>
@@ -17,12 +59,12 @@ export const RecordingModal = () => {
           <ModalHeader> Record A Song! </ModalHeader>
           <ModalCloseButton />
           <ModalBody>
-              placeholder for recording
+              <IconButton aria-label='start recording' icon={<AiOutlinePlayCircle width='20vw'/>} onClick={record}/>
           </ModalBody>
 
           <ModalFooter>
             <ButtonGroup>
-              <Button>Upload</Button>
+              <Button onClick={submit}>Upload</Button>
             </ButtonGroup>
           </ModalFooter>
         </ModalContent>
