87fc57c0ef33d732f45d9d7dfad8aaaad6ec8152
Ivan Day <ivanday@utexas.edu>
2023-01-19 11:13:49 -0600

    finish functionality of respondInvitation endpoint

--- pages/api/respondInvitation.ts
+++ pages/api/respondInvitation.ts
@@ -7,6 +7,26 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
+  if (req.method !== 'PUT') {
+    res.status(405).send({message: 'Only PUT requests allowed!'});
+    res.end();
+  }
   const sessionData = await unstable_getServerSession(req, res, authOptions);
-  const userId = await prisma.users.findUnique({where: {email: sessionData?.user?.email}});
+  const user = await prisma.users.findUnique({where: {email: sessionData?.user?.email}, select: {id: true, name: true}});
+  if (req.body.accept) {
+    prisma.roles.create({data: {userId: user?.id, bandId: req.body.bandId, admin: false, name: user?.name}})
+    .then((response) => {
+      return prisma.invitations.delete({where: {bandId: req.body.bandId, userId: user?.id}})
+    })
+    .then((response) => {
+      res.send('Invitation accepted');
+      res.end();
+    })
+  } else {
+    prisma.invitations.delete({where: {bandId: req.body.bandId, userId: user?.id}})
+    .then((response) => {
+      res.send('Invitation deleted');
+      res.end();
+    })
+  }
 }
