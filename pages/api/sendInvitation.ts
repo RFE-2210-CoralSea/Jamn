0d196b5183126469ed9ceb20d6399999fe600299
Ivan Day <ivanday@utexas.edu>
2023-01-19 10:42:14 -0600

    Added error handling for API response

--- pages/api/sendInvitation.ts
+++ pages/api/sendInvitation.ts
@@ -10,8 +10,14 @@ export default async function handler(
     res.end();
   } else {
     const userId = await prisma.users.findUnique({where: {email: req.body.email}, select: {id: true}});
-    const sendInvite = await prisma.invitations.create({data: {userId: userId.id, bandId: req.body.bandId}});
-    res.send('Invitation sent')
-    res.end();
+    prisma.invitations.create({data: {userId: userId.id, bandId: req.body.bandId}})
+    .then((response) => {
+      res.send('Invitation sent')
+      res.end();
+    })
+    .catch((err) => {
+      res.send('Invalid invitation | Please check that the user\' email is valid, that this is not a duplicate invitation, or the band id');
+      res.status(400).end();
+    })
   }
 }
