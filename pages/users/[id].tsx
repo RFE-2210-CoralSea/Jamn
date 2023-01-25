219b4898c5a0dcd2d91393d81e6ff71b158b87f5
Thomas Broadwater <tgbroadh2o@gmail.com>
2023-01-24 20:56:28 -0500

    Display user info

--- pages/users/[id].tsx
+++ pages/users/[id].tsx
@@ -1,7 +1,7 @@
 import { Box, Center, SimpleGrid, Spinner, VStack } from '@chakra-ui/react'
 import Head from 'next/head'
 
-import { NavBar } from 'components'
+import { NavBar, ProfileImage, PersonalDescription } from 'components'
 import prisma from '../../lib/Prisma'
 
 interface FeedProps {
@@ -11,6 +11,15 @@ interface FeedProps {
     id: number,
     name: string,
     picture: string,
+    instruments: {
+      instrument: string,
+      id: number,
+      userId: number
+    }[],
+    roles: {
+      name: string,
+      id: number
+    }[]
   }
 }
 
@@ -23,6 +32,16 @@ export default function UserFeed({ user }: FeedProps) {
       </Head>
       <Box h="100vh" w="100vw" maxW="100%">
         <NavBar />
+        <SimpleGrid columns={1} alignContent="center">
+          <VStack w="100%">
+            <ProfileImage image={user.picture} username={user.name} />
+            <PersonalDescription
+              description={user.bio}
+              instruments={user.instruments}
+              roles={user.roles}
+            />
+          </VStack>
+        </SimpleGrid>
       </Box>
     </>
   )
@@ -32,12 +51,33 @@ export async function getServerSideProps({ params }: any) {
   const user = await prisma.users.findFirst({
     where: {
       id: parseInt(params.id as string)
+    },
+    include: {
+      instruments: true,
+      roles: true,
     }
   })
 
   if (!user) {
     return {
       notFound: true
+    }
+  }
+
+  if (user.roles.length > 0) {
+    for (let i = 0; i < user.roles.length; i++) {
+      const bandNames = await prisma.bands.findUnique({
+        where: {
+          id: user.roles[i].bandId
+        }
+      })
+      if (bandNames) {
+        user.roles[i] = {
+          ...user.roles[i],
+          name: bandNames.name,
+          id: bandNames.id
+        }
+      }
     }
   }
 
