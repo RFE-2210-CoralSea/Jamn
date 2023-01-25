98018ecf7cf1c8edc5330425ac37116b58dda43f
Thomas Broadwater <tgbroadh2o@gmail.com>
2023-01-25 08:41:39 -0500

    Remove editable icons from dynamic user page

--- pages/users/[id].tsx
+++ pages/users/[id].tsx
@@ -1,7 +1,26 @@
-import { Box, Center, SimpleGrid, Spinner, VStack } from '@chakra-ui/react'
+import {
+  Box,
+  Center,
+  VStack,
+  Avatar,
+  Text,
+  Tabs,
+  TabList,
+  Tab,
+  TabPanel,
+  TabPanels,
+  useColorModeValue,
+  List,
+  TagLabel,
+  Tag,
+  Flex,
+  ListItem
+} from '@chakra-ui/react'
+import { GetServerSidePropsContext } from 'next'
 import Head from 'next/head'
+import Link from 'next/link'
 
-import { NavBar, ProfileImage, PersonalDescription } from 'components'
+import { NavBar } from 'components'
 import prisma from '../../lib/Prisma'
 
 interface FeedProps {
@@ -24,33 +43,83 @@ interface FeedProps {
 }
 
 export default function UserFeed({ user }: FeedProps) {
-  console.log(user)
   return (
     <>
       <Head>
-        <title>Username</title>
+        <title>{user.name}&apos;s Page</title>
       </Head>
       <Box h="100vh" w="100vw" maxW="100%">
         <NavBar />
-        <SimpleGrid columns={1} alignContent="center">
+        <Center>
           <VStack w="100%">
-            <ProfileImage image={user.picture} username={user.name} />
-            <PersonalDescription
-              description={user.bio}
-              instruments={user.instruments}
-              roles={user.roles}
+            <Avatar
+              src={user.picture}
+              boxShadow="dark-lg"
+              border="1px solid black"
+              objectFit="cover"
+              boxSize="15rem"
+              mt="10rem"
             />
+
+            <Text textAlign="center" fontSize="3xl" fontWeight="bold">
+              {user.name}
+            </Text>
+
+            <Box w="16rem">
+              <Tabs variant="soft-rounded" colorScheme={useColorModeValue('blue', 'green')}>
+                <TabList>
+                  <Tab>Bio</Tab>
+                  <Tab>Bands</Tab>
+                  <Tab>Instruments</Tab>
+                </TabList>
+                <TabPanels>
+                  <TabPanel>
+                    <Text fontSize="xl" fontWeight="bold">{user.bio}</Text>
+                  </TabPanel>
+                  <TabPanel>
+                    <Center>
+                      <List fontSize="lg" fontWeight="bold">
+                        {user.roles.map((role) => {
+                          return (
+                            <Flex key={role.id} justifyContent="space-between" mb="1rem">
+                              <Tag
+                                size="xl"
+                                colorScheme={useColorModeValue('blue', 'green')}
+                                borderRadius="full"
+                              >
+                                <Avatar size="sm" mr={2} />
+                                <TagLabel fontWeight="bold" mr={3} key={role.name}>
+                                  <Link href={`/bands/${role.id}`}>{role.name}</Link>
+                                </TagLabel>
+                              </Tag>
+                            </Flex>
+                          )
+                        })}
+                      </List>
+                    </Center>
+                  </TabPanel>
+                  <TabPanel>
+                    <List fontSize="lg" textAlign="center" fontWeight="bold">
+                      {user.instruments.map((instrument) => {
+                        return <ListItem key={instrument.id}>• {instrument.instrument}</ListItem>
+                      })}
+                    </List>
+                  </TabPanel>
+                </TabPanels>
+              </Tabs>
+            </Box>
           </VStack>
-        </SimpleGrid>
+        </Center>
       </Box>
     </>
   )
 }
 
-export async function getServerSideProps({ params }: any) {
+export async function getServerSideProps({ params }: GetServerSidePropsContext) {
+  // query DB for user
   const user = await prisma.users.findFirst({
     where: {
-      id: parseInt(params.id as string)
+      id: parseInt(params?.id as string)
     },
     include: {
       instruments: true,
@@ -64,6 +133,7 @@ export async function getServerSideProps({ params }: any) {
     }
   }
 
+  // query for a user's bands
   if (user.roles.length > 0) {
     for (let i = 0; i < user.roles.length; i++) {
       const bandNames = await prisma.bands.findUnique({
